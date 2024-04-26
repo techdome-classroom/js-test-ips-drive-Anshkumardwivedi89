57d37f511f23f9dcaef06de45318019bf6cdbe88
Anshkumardwivedi89 <anshdwivedi2002@gmail.com>
2024-04-26 13:16:53 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,9 +1,24 @@
-function smallestMissingPositiveInteger(nums) {
 
+  function smallestMissingPositiveInteger(nums) {
 
     // Implement the function smallest_missing_positive_integer
+      nums = nums.filter(num => num > 0);  // filter the numbers greater than zero
+  
+      for (let i = 0; i < nums.length; i++) {
+          let pos = Math.abs(nums[i]) - 1;   // calculate index of the values
+          if (pos < nums.length && nums[pos] > 0) {
+              nums[pos] = -nums[pos];
+          }
+      }
+      for (let i = 0; i < nums.length; i++) {
+          if (nums[i] > 0) {
+              return i + 1;
+          }
+      }
+      return nums.length + 1;
   
   }
   
   module.exports = smallestMissingPositiveInteger;
-
+  
+ 
--- program2.js
+++ program2.js
@@ -1,7 +1,30 @@
+
 function longestSubstring(s) {
-
-
     // Implementation of longestSubstring function
-}
+        let longestSubStringsize = 0; //variable to calculate the substring size
+   
+        for (let i = 0; i < s.length; i++) {
+            let currentSet = new Set();      //Set to store and check incoming strings
+   
+            for (let x = i; x < s.length; x++) {
+               
+                if (currentSet.has(s[x])) {  // check if set contains it already 
+                   
+                    break;
+                } else {
+                   
+                    currentSet.add(s[x]);  // if does not contain add to the set
+                }
+            }
+            //calculate longest substring size by comparing to max function
+            longestSubStringsize = Math.max(
+                longestSubStringsize,
+                currentSet.size
+            );                              
+        }
+   
+        return longestSubStringsize;
 
+   
+}
 module.exports = { longestSubstring };
